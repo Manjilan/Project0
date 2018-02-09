2c57bacbe1f318aa9d5e4ffb15777a84ee769478
Manjilan <nakarmimanjila@yahoo.com>
2018-02-09 00:15:29 -0800

    scrolling request

--- js/api.js
+++ js/api.js
@@ -2,6 +2,7 @@
 // API
 // -----------------------------
 // define globals
+var offset = 0;
 var marvel_endpoint = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f7237d2d6b84524fee8eba2943790ea6&hash=d8358291a51527982a889a850adab3ac";
   console.log("Let's get coding!");
   // CODE IN HERE!
@@ -11,7 +12,8 @@ var marvel_endpoint = "http://gateway.marvel.com/v1/public/characters?ts=1&apike
     method: "GET",
     url: marvel_endpoint,
     data: {
-        limit: 20
+        limit: 20,
+        offset: offset
     },
     success: function(response){
       $(response.data.results).each(function(){
@@ -23,3 +25,20 @@ var marvel_endpoint = "http://gateway.marvel.com/v1/public/characters?ts=1&apike
       },
 
     })
+
+$(window).on('scroll', function(e) {
+      offset += 20;
+      $.ajax({
+        method: "GET",
+        url: marvel_endpoint,
+        data: {
+            limit: 20,
+            offset: offset
+        },
+        success: function(response){
+          $(response.data.results).each(function(){
+            $("main").append(`<div><p>${this.name}</p><img src="${this.thumbnail.path}/standard_fantastic.jpg"></div>`);
+          })
+        }
+
+      })  })
